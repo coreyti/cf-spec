5393bc84b7be64192a3d11b60b584d7cad73a3a2
Corey Innis <coreyti@gmail.com>
2017-09-16 14:09:34 -0700

    Initial implementation: rework BOSH command
    It now handles:
    
    - bosh://
    - bosh://env
    - bosh://env/deployment
    - bosh://env/deployment#group
    - bosh://env/deployment#group/index (or guid)

--- lib/cf/spec/transports/bosh.rb
+++ lib/cf/spec/transports/bosh.rb
@@ -4,7 +4,9 @@ module CF::Spec
   module Transports
     class BOSH < Base
       def command(params)
-        "bosh -e #{config.host} -d #{deployment} #{rebuild(params)}"
+        ['bosh', globals, rebuild(params)]
+          .delete_if { |part| part == '' }
+          .join(' ')
       end
 
       private
@@ -16,7 +18,34 @@ module CF::Spec
       # ---
 
       def rebuild(params)
-        params.split(' ').insert(1, config.frag).compact.join(' ')
+        params.split(' ').insert(1, target)
+          .delete_if { |part| part == '' }
+          .compact.join(' ')
+      end
+
+      def globals
+        [].tap do |result|
+          result << "-e #{address[:environment]}" if address[:environment]
+          result << "-d #{address[:deployment]}"  if address[:deployment]
+        end.join(' ')
+      end
+
+      def target
+        [address[:group], address[:index]].compact.join('/')
+      end
+
+      # ---
+
+      def address
+        @target ||= {}.tap do |result|
+          path = (config.path || '').split('/').delete_if { |part| part == '' }
+          frag = (config.frag || '').split('/').delete_if { |part| part == '' }
+
+          result[:environment] = config.host if config.host
+          result[:deployment]  = path[0]
+          result[:group]       = frag[0]
+          result[:index]       = frag[1]
+        end
       end
     end
   end
